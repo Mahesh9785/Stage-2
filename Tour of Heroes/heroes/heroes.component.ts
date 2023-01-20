51a833acda1dd770fddaad87ad273fdab8feb55a
Mahesh Sharma <76154347+Mahesh9785@users.noreply.github.com>
2023-01-20 19:08:01 +0530

    Add files via upload

--- Tour of Heroes/heroes/heroes.component.ts
+++ Tour of Heroes/heroes/heroes.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
 import { Hero } from 'src/app/hero';
-import { HEROES } from '../mock-heroes';
+import { ServiceService } from '../service.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
-
-
 export class HeroesComponent {
-  public aa= prompt("What is your name ?");
-  t = this.aa;
+  // input:string=prompt("What is your name?") as string;
+   title="My Tour of Heroes";
 
-  heroes = HEROES;
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  heroes: Hero[]=[];
+  selectedHero?:Hero;
+  onSelect(hero:Hero):void{
+    this.selectedHero= hero;
+  }
+
+  getHeroes(): void {
+    this.serviceService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private serviceService:ServiceService){
+
+  }
+
+  ngOnInit(): void{
+    this.getHeroes();
   }
 }
-
